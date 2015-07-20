0f6936a2fbe4b96edb04dd4d281b441781bc66d6
Lemgruber <bernardo.lemgruber@vtex.com.br>
2015-07-20 14:14:37 -0300

    Estrutura a Home do tema Fera Fashion

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import { dispatcher } from 'sdk';
 import DocumentTitle from 'react-document-title';
 import { RouteHandler, State } from 'react-router';
+import Header from './header/Header';
+import Footer from './footer/Footer';
 import { IntlMixin } from 'react-intl';
 
 let App = React.createClass({
--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Navigation } from 'react-router';
-import Style from './header.less'; // eslint-disable-line
+import Style from './Header.less'; // eslint-disable-line
 import {
   Navbar, Nav, NavItem
 } from 'react-bootstrap';
@@ -9,9 +9,13 @@ let Header = React.createClass({
   mixins: [ Navigation ],
   render() {
     return (
-      <Navbar staticTop={true} brand='Comuna' inverse toggleNavKey={0}>
+      <Navbar staticTop="true" fluid="true" brand="FERA fashion" className="navbar" toggleNavKey={0}>
         <Nav right eventKey={0}> {/* This is the eventKey referenced */}
-          <NavItem eventKey='1' onClick={() => this.transitionTo('home')}>Home</NavItem>
+          <NavItem eventKey='1' onClick={() => this.transitionTo('home')}>home</NavItem>
+          <NavItem eventKey='2' onClick={() => this.transitionTo('blusas')}>blusas</NavItem>
+          <NavItem eventKey='3' onClick={() => this.transitionTo('regatas')}>regatas</NavItem>
+          <NavItem eventKey='4' onClick={() => this.transitionTo('shorts')}>shorts</NavItem>
+          <NavItem eventKey='5' onClick={() => this.transitionTo('bodys')}>bodys</NavItem>
         </Nav>
       </Navbar>
     );
