cee0187c1df9ac7ae0a41653347ceb3fb386efd6
Breno Calazans <breno@vtex.com.br>
2015-08-11 17:00:34 -0300

    Make Header in React
    Fix #6

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -4,7 +4,21 @@ import style from 'styles/components/header/Header.less'; // eslint-disable-line
 class Header extends React.Component {
   render() {
     return (
-      <div>Header</div>
+      <div className="header row-fluid clearfix">
+        <div className="col-xs-1">
+          <div className="v-icon__hamburger"></div>
+        </div>
+
+        <h1 className="header-brand col-xs-7">Fera fashion</h1>
+
+        <div className="col-xs-1">
+          <div className="v-icon__search"></div>
+        </div>
+
+        <div className="col-xs-2">
+          <div className="v-icon__cart"></div>
+        </div>
+      </div>
     );
   }
 }
