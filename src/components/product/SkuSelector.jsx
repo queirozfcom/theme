a9b91d75031981095aaa518af279ea83b2d75505
bellacorbo <bella.corbo@gmail.com>
2015-08-19 17:43:48 -0300

    Create sku availability check function

--- src/components/product/SkuSelector.jsx
+++ src/components/product/SkuSelector.jsx
@@ -8,9 +8,40 @@ class SkuSelector extends React.Component {
     return 'v-dream__size-selector--unavailable';
   }
 
+  checkVariations(sku) {
+    if (sku.length <= 1) {
+      return <div></div>;
+    }
+    return (
+      <div className="row v-dream__selector-section--highlight">
+        <div className="row v-dream__selector-row">
+          <h3 className="v-dream__selector__title col-xs-11 col-xs-offset-1">Selecione o tamanho:</h3>
+          <div>
+            {mock.skus.map( (sku, index) => {
+              if (index == 0)
+                return (
+                  <button key={sku.id} className={ this.changeAvailability(sku.availability) + ' col-xs-offset-1 col-xs-2'}>
+                    { sku.size }
+                  </button> );
+              return (
+                <button key={sku.id} className={ this.changeAvailability(sku.availability) + ' col-xs-2'}>
+                  { sku.size }
+                </button> );
+            }
+            )}
+          </div>
+        </div>
+      </div>
+    );
+  }
+
   render() {
     return (
+      <div>
+        { this.checkVariations(mock.skus) }
+      </div>
     );
   }
 }
+
 export default SkuSelector;
