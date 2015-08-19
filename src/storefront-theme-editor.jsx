0cde115071746834c979cb17d56e22598d1c0a2e
Breno Calazans <breno@vtex.com.br>
2015-08-19 14:08:05 -0300

    Fix hot reload

--- src/storefront-theme-editor.jsx
+++ src/storefront-theme-editor.jsx
@@ -14,9 +14,3 @@ let components = [
 ];
 
 dispatcher.actions.ComponentActions.register(components);
-
-// Enable react hot loading with external React
-// see https://github.com/gaearon/react-hot-loader/tree/master/docs#usage-with-external-react
-if (module.hot) {
-  window.RootInstanceProvider = require('react-hot-loader/Injection').RootInstanceProvider;
-}
