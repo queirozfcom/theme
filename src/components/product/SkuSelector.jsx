741bea21dd2ef86363250fb7d3b46b0d4f56b174
bellacorbo <bella.corbo@gmail.com>
2015-08-20 15:58:58 -0300

    Add color selector

--- src/components/product/SkuSelector.jsx
+++ src/components/product/SkuSelector.jsx
@@ -40,10 +40,8 @@ class SkuSelector extends React.Component {
       return <div></div>;
     }
     return (
-      <div className="row v-dream__selector-section--highlight">
-        <div className="row v-dream__selector-row">
-          <h3 className="v-dream__selector__title col-xs-11 col-xs-offset-1">Selecione o tamanho:</h3>
           <div>
+            <h3 className="v-dream__selector__title col-xs-11 col-xs-offset-1">Selecione o tamanho:</h3>
             {mock.skus.map( (sku, index) => {
               if (index === 0) {
                 return (
@@ -55,18 +53,41 @@ class SkuSelector extends React.Component {
                 <button key={sku.id} className={ this.changeAvailability(sku.availability) + ' col-xs-2'}>
                   { sku.size }
                 </button> );
-            }
+              }
             )}
           </div>
+    );
+  }
+
+  displayColorVariations() {
+    return (
+      <div>
+        <h3 className="v-dream__selector__title col-xs-11 col-xs-offset-1">Selecione a cor:</h3>
+        <div className="v-dream__color-selector__wrapper">
+          <button className="v-dream__color-selector col-xs-2 col-xs-offset-1"></button>
+          <button className="v-dream__color-selector col-xs-2"></button>
+          <button className="v-dream__color-selector col-xs-2"></button>
         </div>
       </div>
-    );
+    )
   }
 
   render() {
     return (
       <div>
-        { this.checkVariations(mock.skus) }
+        <div className="row v-dream__selector-section--highlight">
+          <div className="row v-dream__selector-row">
+            <div>
+              { this.checkVariations(mock.skus) }
+            </div>
+          </div>
+          <div className="row v-dream__selector-row">
+              { this.displayColorVariations() }
+          </div>
+          <div className="row">
+            <p className="col-xs-12 v-dream__selector-section__alert-text">Escolha um tamanho e cor para adicionar o produto ao carrinho.</p>
+          </div>
+        </div>
       </div>
     );
   }
