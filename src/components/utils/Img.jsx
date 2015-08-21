5bfde1905dadc8e514ef243819001fcb06bbce45
Breno Calazans <breno@vtex.com.br>
2015-08-21 16:08:57 -0300

    Remove Img feature

--- src/components/utils/Img.jsx
+++ src/components/utils/Img.jsx
@@ -3,8 +3,7 @@ import { dispatcher } from 'sdk';
 
 class Img extends React.Component {
   static defaultProps = {
-    ShopStore: dispatcher.stores.ShopStore.getState(),
-    prefetchOnHover: false
+    ShopStore: dispatcher.stores.ShopStore.getState()
   }
 
   static propTypes = {
@@ -19,22 +18,12 @@ class Img extends React.Component {
     return `http://${accountName}.vteximg.com.br`;
   }
 
-  onMouseOver() {
-    if (this.props.prefetchOnHover) {
-      let path = this.props.src.replace('#width#', 490).replace('#height#', 490);
-      path = this.getBaseUrl(this.props) + path;
-      const prefetchImage = new Image();
-      prefetchImage.src = path;
-    }
-  }
-
   render() {
     let path = this.props.src.replace('#width#', this.props.width).replace('#height#', this.props.width);
     path = this.getBaseUrl(this.props) + path;
 
     return (
       <img
-        onMouseOver={this.onMouseOver}
         className={this.props.className}
         src={path}
         alt={this.props.alt}
