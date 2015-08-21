94141bed0441fe3ac71704282e9393a33e126565
Breno Calazans <breno@vtex.com.br>
2015-08-21 13:49:01 -0300

    Improve Shelf code

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -10,7 +10,8 @@ import Shelf from 'components/shelf/Shelf';
 import 'styles/pages/HomePage.less';
 
 @connectToStores([
-  dispatcher.stores.SettingsStore
+  dispatcher.stores.SettingsStore,
+  dispatcher.stores.EditorStore
 ])
 class HomePage extends React.Component {
   static contextTypes = State.contextTypes
@@ -24,18 +25,15 @@ class HomePage extends React.Component {
   render() {
     const SettingsStore = this.props.SettingsStore;
     let bannerSettings = SettingsStore.getIn(['home', 'home-banner', 'settings']);
-    bannerSettings = bannerSettings ? bannerSettings.toJS() : bannerSettings;
     let shelf1Settings = SettingsStore.getIn(['home', 'home-shelf-1', 'settings']);
-    shelf1Settings = shelf1Settings ? shelf1Settings.toJS() : shelf1Settings;
     let shelf2Settings = SettingsStore.getIn(['home', 'home-shelf-2', 'settings']);
-    shelf2Settings = shelf2Settings ? shelf2Settings.toJS() : shelf2Settings;
 
     return (
       <div>
         <Header/>
-        <Banner {...bannerSettings}/>
-        <Shelf {...shelf1Settings} id="home-shelf-1" route="home"/>
-        <Shelf {...shelf2Settings} id="home-shelf-2" route="home"/>
+        <Banner id="home-banner" route="home" settings={bannerSettings} EditorStore={this.props.EditorStore}/>
+        <Shelf id="home-shelf-1" route="home" settings={shelf1Settings} EditorStore={this.props.EditorStore}/>
+        <Shelf id="home-shelf-2" route="home" settings={shelf2Settings} EditorStore={this.props.EditorStore}/>
         <Policies/>
         <Newsletter/>
         <Footer/>
