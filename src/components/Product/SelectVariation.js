9bfedd1f71b81c32f6a071d9332069afbd545b78
bellacorbo <bella.corbo@gmail.com>
2015-08-25 16:15:00 -0300

    Get variation slug from sku

--- src/components/Product/SelectVariation.js
+++ src/components/Product/SelectVariation.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import VariationButton from './VariationButton';
 
 class SelectVariation extends React.Component {
   changeAvailability(availability) {
@@ -12,15 +13,38 @@ class SelectVariation extends React.Component {
     if (this.props.skus.length <= 1) {
       return null;
     }
+
+    let getSkuGroups = function(skus, facet = 0) {
+      let grupo = {};
+
+      each(skus, function(sku) {
+        let chave = sku.properties[facet].facet.values[0];
+
+        try {
+          grupo[chave].push(sku);
+        } catch(error) {
+          grupo[chave] = [sku];
+        }
+      });
+
+      return grupo;
+    };
+
+    console.log(getSkuGroups(this.props.skus));
+    console.log(getSkuGroups(getSkuGroups(this.props.skus).PP, 1));
+
+    let groups = getSkuGroups(this.props.skus);
     return (
-      <div className="col-xs-12" margin-bottom='10px'>
-        <h3 className="v-dream__selector__title">Selecione o {this.props.variation}:</h3>
-        <div className="v-dream__size-selector__wrapper">
-        {this.props.skus.map((sku) =>
-          <button key={sku.id} className={ this.changeAvailability(sku.availability) + ' col-xs-2'}>
-            { sku[this.props.variation] }
-          </button>
-        )}
+      <div className="col-xs-12">
+        <h3 className="v-dream__selector__title col-xs-11">Selecione o {this.props.variation}:</h3>
+        <div className="v-dream__size-selector__wrapper col-xs-11">
+        {Object.keys(groups).map(function(group) {
+          return (
+            <div key={group}>
+              <VariationButton skus={groups[group]} value={group} getSkuGroups={getSkuGroups} />
+            </div>
+          );
+        })}
         </div>
       </div>
     );
--- src/components/Product/SkuSelector.js
+++ src/components/Product/SkuSelector.js
@@ -16,16 +16,17 @@ class SkuSelector extends React.Component {
   render() {
     let classes = 'v-dream__selector-section col-xs-12';
     classes = this.props.validationError ? ('v-dream__selector-section--highlight ' + classes) : classes;
+    let variation = this.props.skus[0].properties[0].facet.slug;
 
     return (
       <div className="row">
         <div id="selector" className={classes}>
           <div className="v-dream__selector-row row-fluid">
-            <SelectVariation skus={this.props.skus} variation="tamanho"/>
+            <SelectVariation skus={this.props.skus} variation={variation}/>
           </div>
             { this.props.validationError ?
               <div className="row-fluid">
-                <p className="col-xs-offset-1 col-xs-10 v-dream__selector-section__alert-text">Escolha um tamanho e cor para adicionar o produto ao carrinho.</p>
+                <p className="col-xs-offset-1 col-xs-10 v-dream__selector-section__alert-text">Escolha um { variation } para adicionar o produto ao carrinho.</p>
               </div>
               : null}
         </div>
