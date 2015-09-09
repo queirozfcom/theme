48dbfd93cc60beba848ac94fc0d11cbe0689f70e
bellacorbo <bella.corbo@gmail.com>
2015-09-09 19:48:29 -0300

    Apply class manipulation for color variation

--- src/components/Product/VariationButton.js
+++ src/components/Product/VariationButton.js
@@ -4,26 +4,41 @@ class VariationButton extends React.Component {
 
   changeState = (ev) => {
     ev.preventDefault();
-    this.props.changeVariationState(this.props.variationName, this.props.value);
     this.props.addFacet(this.props.variationName, this.props.value);
   }
 
-  changeAvailability = (isActive) => {
+  changeAvailability = (isActive, variationName) => {
+    if(variationName === 'Tamanho') {
+      if (this.props.getAvailability(this.props.value) > 0) {
+        if(isActive)
+          return 'v-dream__size-selector--active ';
+        return 'v-dream__size-selector ';
+      }
+      return 'v-dream__size-selector--unavailable ';
+  } else if(variationName === 'Cor') {
     if (this.props.getAvailability(this.props.value) > 0) {
       if(isActive)
-        return 'v-dream__size-selector--active ';
-      return 'v-dream__size-selector';
+        return 'v-dream__color-selector--active ';
+      return 'v-dream__color-selector ';
     }
-    return 'v-dream__size-selector--unavailable';
+    return 'v-dream__color-selector--unavailable ';
+}
+}
+
+  displayValue = (variationName) => {
+    if(variationName === 'Tamanho') {
+      return this.props.value;
+    } else {
+      return null;
+    }
   }
 
   render() {
     let isActive = this.props.isActive;
-  //  let skus = this.props.skus;
 
     return (
-      <button className={ this.changeAvailability(isActive) + ' col-xs-2' } onTouchTap={this.changeState}>
-        { this.props.value }
+      <button className={ this.changeAvailability(isActive, this.props.variationName) + ' col-xs-2' } onTouchTap={this.changeState}>
+        { this.displayValue(this.props.variationName) }
       </button>
     );
   }
