024c0e1a5491d28a298e3ba9009e6c964ba13a73
bellacorbo <bella.corbo@gmail.com>
2015-09-29 15:17:47 -0300

    Remove unused prop

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -1,4 +1,3 @@
-
 import React from 'react';
 import './Product.less';
 import Img from 'utils/Img';
--- src/components/Product/SkuSelector.js
+++ src/components/Product/SkuSelector.js
@@ -6,7 +6,7 @@ import { storefront } from 'sdk';
 @storefront({
   name: 'SkuSelector@vtex.storefront-theme',
   title: 'SkuSelector',
-  editable: true 
+  editable: true
 })
 class SkuSelector extends React.Component {
 
@@ -14,7 +14,6 @@ class SkuSelector extends React.Component {
   render() {
     let classes = 'v-dream__selector-section col-xs-12';
     classes = this.props.validationError ? ('v-dream__selector-section--highlight ' + classes) : classes;
-    let variationName = this.props.skuVariations[0].name;
 
     return (
       <div className="row">
@@ -23,7 +22,7 @@ class SkuSelector extends React.Component {
           this.props.skuVariations.map((variationType) => {
             return (
               <div className="v-dream__selector-row row-fluid" key={variationType.name}>
-                <SelectVariation skus={this.props.skus} variationName={variationName} removeFacet={this.props.removeFacet} facets={this.props.facets}
+                <SelectVariation skus={this.props.skus} removeFacet={this.props.removeFacet} facets={this.props.facets}
                                  skuVariations={variationType} addFacet={this.props.addFacet} getAvailability={this.props.getAvailability}
                                  getSkuVariations={this.getSkuVariations} changeAvailability={this.props.changeAvailability}/>
               </div>
