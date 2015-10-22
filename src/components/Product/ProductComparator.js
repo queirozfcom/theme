f26e3ec25250667ae8dcfa8f5d1e12ad2cb0c77b
leandrooriente <leandroriente@gmail.com>
2015-10-22 13:33:53 -0200

    Add some layout improvements to product comparator

--- src/components/Product/ProductComparator.js
+++ src/components/Product/ProductComparator.js
@@ -7,26 +7,37 @@ class ProductComparator extends React.Component {
       <div className="product-comparator">
         <h3>Compare produtos semelhantes</h3>
         <div className="product-comparator-table-container">
-          <table className="table table-bordered">
+          <table className="table">
             <thead>
               <tr>
-                <td></td>
-                <td>iPhone 6</td>
-                <td>iPhone 5s</td>
-                <td>iPhone 5</td>
+                <td className="clear-td"></td>
+                <td className="product-comparator-name">
+                  <img className="product-comparator-image" src="http://www.case-custom.com/media/catalog/product/cache/1/image/500x/9df78eab33525d08d6e5fb8d27136e95/i/p/iphone6-clear-silicone-case-slim-tough.jpg" />
+                  <strong>iPhone 6</strong>
+                </td>
+
+                <td className="product-comparator-name">
+                  <img className="product-comparator-image" src="http://www.case-custom.com/media/catalog/product/cache/1/image/500x/9df78eab33525d08d6e5fb8d27136e95/i/p/iphone6-clear-silicone-case-slim-tough.jpg" />
+                  <strong>iPhone 5s</strong>
+                </td>
+
+                <td className="product-comparator-name">
+                  <img className="product-comparator-image" src="http://www.case-custom.com/media/catalog/product/cache/1/image/500x/9df78eab33525d08d6e5fb8d27136e95/i/p/iphone6-clear-silicone-case-slim-tough.jpg" />
+                  <strong>iPhone 5</strong>
+                </td>
               </tr>
             </thead>
             <tfoot>
               <tr>
-                <td></td>
-                <td><button className="btn btn-xs btn-primary">Adicionar ao carrinho</button></td>
-                <td><button className="btn btn-xs btn-primary">Adicionar ao carrinho</button></td>
-                <td><button className="btn btn-xs btn-primary">Adicionar ao carrinho</button></td>
+                <td className="clear-td"></td>
+                <td><button className="btn btn-xs btn-success">Adicionar ao carrinho</button></td>
+                <td><button className="btn btn-xs btn-success">Adicionar ao carrinho</button></td>
+                <td><button className="btn btn-xs btn-success">Adicionar ao carrinho</button></td>
               </tr>
             </tfoot>
             <tbody>
               <tr>
-                <td className="">
+                <td className="active">
                   <strong>Preço</strong>
                 </td>
                 <td>
@@ -41,7 +52,7 @@ class ProductComparator extends React.Component {
               </tr>
 
               <tr>
-                <td>
+                <td className="active">
                   <strong>Memória</strong>
                 </td>
                 <td>
@@ -56,7 +67,7 @@ class ProductComparator extends React.Component {
               </tr>
 
               <tr>
-                <td>
+                <td className="active">
                   <strong>Câmera</strong>
                 </td>
                 <td>
