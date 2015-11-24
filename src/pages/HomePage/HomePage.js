9aa8d45f21748f5b3333487151cde265e3c619ba
Breno Calazans <breno@vtex.com.br>
2015-11-24 12:15:35 -0200

    Define areas

--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -1,24 +1,12 @@
 import React from 'react';
-import { actions, stores, utils } from 'sdk';
+import { actions, stores, utils, Area } from 'sdk';
 import Header from 'components/Header/Header';
 import Footer from 'components/Footer/Footer';
 import Newsletter from 'components/Newsletter/Newsletter';
 import Policies from 'components/Policies/Policies';
 import './HomePage.less';
 
-@utils.connectToStores()
 class HomePage extends React.Component {
-  static getStores() {
-    return [stores.ComponentStore];
-  }
-
-  static getPropsFromStores() {
-    return {
-      banner: stores.ComponentStore.getState().getIn(['Banner@vtex.storefront-banner']),
-      shelf: stores.ComponentStore.getState().getIn(['Shelf@vtex.storefront-shelf'])
-    };
-  }
-
   componentWillMount() {
     let currentURL = (window.location.pathname + window.location.search);
     if (!stores.ResourceStore.getState().get(currentURL)) {
@@ -27,15 +15,11 @@ class HomePage extends React.Component {
   }
 
   render() {
-    let Banner = this.props.banner ? this.props.banner.get('constructor') : null;
-    let Shelf = this.props.shelf ? this.props.shelf.get('constructor') : null;
-
     return (
       <div>
         <Header/>
-        {Banner ? <Banner id="home-banner" route="home"/> : null}
-        {Shelf ? <Shelf id="home-shelf-1" route="home"/> : null}
-        {Shelf ? <Shelf id="home-shelf-2" route="home"/> : null}
+        <Area id="home/banner"/>
+        <Area id="home/shelf1"/>
         <Policies/>
         <Newsletter/>
         <Footer/>
