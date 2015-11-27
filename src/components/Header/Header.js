60de375b364887bddca3fbe8e3ea0927b9ddb92a
Thor Amorim <thoramorim@gmail.com>
2015-11-26 22:18:58 -0200

    Add Menu on header component

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import ReactCSSTransitionGroup from 'react-addons-css-transition-group';
+import { Area } from 'sdk';
 import './Header.less';
 import { Link } from 'react-router';
 import SVGIcon from 'utils/SVGIcon';
@@ -7,10 +9,27 @@ import searchIcon from 'assets/icons/search.svg';
 import cartIcon from 'assets/icons/cart.svg';
 
 class Header extends React.Component {
+  state = {
+    isMenuOpen: false
+  }
+
+  handleMenuTap = () => {
+    this.setState({ isMenuOpen: !this.state.isMenuOpen});
+  }
+
   render() {
+    let menu = this.state.isMenuOpen ?
+      (
+        <Area
+          id={`${this.props.areaPath}/menu`}
+          key="Menu"
+          toggleMenu={this.handleMenuTap}
+        />
+      ) : null;
+
     return (
       <div className="Header clearfix">
-        <div className="col-xs-1">
+        <div className="col-xs-1" onTouchTap={this.handleMenuTap}>
           <SVGIcon className="Header__icon" svg={hamburgerIcon} width={18} height={18} fill="#153243"/>
         </div>
 
@@ -25,6 +44,14 @@ class Header extends React.Component {
         <div className="col-xs-1">
           <SVGIcon className="Header__icon" svg={cartIcon} width={18} height={18} fill="#153243"/>
         </div>
+
+        <ReactCSSTransitionGroup
+          transitionName="Menu"
+          transitionEnterTimeout={200}
+          transitionLeaveTimeout={250}
+        >
+          { menu }
+        </ReactCSSTransitionGroup>
       </div>
     );
   }
