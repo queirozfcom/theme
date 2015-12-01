9d35171e229ce55ebc2a151791afc96904c11b3d
Daniel Fosco <daniel.fosco@vtex.com.br>
2015-12-01 20:56:25 -0200

    Fix layout for header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -9,21 +9,21 @@ import cartIcon from 'assets/icons/cart.svg';
 class Header extends React.Component {
   render() {
     return (
-      <div className="v-header clearfix">
+      <div className="Header clearfix">
         <div className="col-xs-1">
-          <SVGIcon className="v-header__icon" svg={hamburgerIcon} width={18} height={18} fill="#153243"/>
+          <SVGIcon className="Header__icon" svg={hamburgerIcon} width={18} height={18} fill="#153243"/>
         </div>
 
-        <h1 className="v-header__brand col-xs-7">
+        <h1 className="Header__brand col-xs-8 col-xs-push-1">
           <Link to="/">Fera fashion</Link>
         </h1>
 
         <div className="col-xs-1">
-          <SVGIcon className="v-header__icon" svg={searchIcon} width={15} height={18} fill="#153243"/>
+          <SVGIcon className="Header__icon" svg={searchIcon} width={15} height={18} fill="#153243"/>
         </div>
 
-        <div className="col-xs-2">
-          <SVGIcon className="v-header__icon" svg={cartIcon} width={18} height={18} fill="#153243"/>
+        <div className="col-xs-1">
+          <SVGIcon className="Header__icon" svg={cartIcon} width={18} height={18} fill="#153243"/>
         </div>
       </div>
     );
