c99cee3a283cb1789dfd12751ad89cd00228f741
Daniel Fosco <daniel.fosco@vtex.com.br>
2015-12-01 21:03:21 -0200

    Refactor Newsletter CSS to use BEM syntax

--- src/components/Newsletter/Newsletter.js
+++ src/components/Newsletter/Newsletter.js
@@ -4,13 +4,13 @@ import './Newsletter.less';
 class Newsletter extends React.Component {
   render() {
     return (
-      <div className="v-newsletter row-fluid">
-        <p className="v-newsletter__title col-xs-3">Assine nossa newsletter:</p>
+      <div className="Newsletter row-fluid">
+        <p className="Newsletter__title col-xs-3">Assine nossa newsletter:</p>
         <div className="row-fluid col-xs-9">
-          <div className="v-newsletter__form input-group">
-            <input type="e-mail" className="v-newsletter__form-input form-control" placeholder="e-mail"/>
+          <div className="Newsletter__form input-group">
+            <input type="e-mail" className="Newsletter__form-input form-control" placeholder="e-mail"/>
             <span className="input-group-btn">
-              <button className="v-newsletter__form-button btn btn-default" type="button">
+              <button className="Newsletter__form-button btn btn-default" type="button">
                 Assinar
               </button>
             </span>
