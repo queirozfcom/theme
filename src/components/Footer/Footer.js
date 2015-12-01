18b9ed0d646bdaa9be827d0705621128a923bea1
Daniel Fosco <daniel.fosco@vtex.com.br>
2015-12-01 20:48:05 -0200

    Refactor Footer CSS to use BEM syntax Improve footer layout and semantics

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -8,23 +8,21 @@ import pinterestIcon from 'assets/icons/pinterest.svg';
 class Footer extends React.Component {
   render() {
     return (
-      <div className="v-footer clearfix">
+      <div className="Footer clearfix">
         <div className="col-xs-10">
-          <ul className="v-footer__menu">
-            <li><a href="#" className="v-footer__menu-link">Categorias</a></li>
-            <li><a href="#" className="v-footer__menu-link">FAQ</a></li>
-            <li><a href="#" className="v-footer__menu-link">Sobre</a></li>
-            <li><a href="#" className="v-footer__menu-link">Termos de uso</a></li>
+          <ul className="Footer__menu">
+            <li className="Footer__item"><a href="#" className="Footer__menu-link">Categorias</a></li>
+            <li className="Footer__item"><a href="#" className="Footer__menu-link">FAQ</a></li>
+            <li className="Footer__item"><a href="#" className="Footer__menu-link">Sobre</a></li>
+            <li className="Footer__item"><a href="#" className="Footer__menu-link">Termos de uso</a></li>
           </ul>
-          <div className="v-footer__address">
-            <p>Praia de Botafogo, 518, Sobreloja, Botafogo. Rio de Janeiro, RJ - 22250-040</p>
-          </div>
+          <span className="Footer__address">Praia de Botafogo, 518, Sobreloja, Botafogo. Rio de Janeiro, RJ - 22250-040</span>
         </div>
 
-        <div className="v-footer__social-icons col-xs-2">
-          <SVGIcon className="v-footer__instagram" svg={instagramIcon} width={33} height={33} fill="#FFFFFF"/>
-          <SVGIcon className="v-footer__facebook" svg={facebookIcon} width={33} height={33} fill="#FFFFFF"/>
-          <SVGIcon className="v-footer__pinterest" svg={pinterestIcon} width={33} height={33} fill="#FFFFFF"/>
+        <div className="Footer__social-icons col-xs-2">
+          <SVGIcon className="Footer__icon" svg={instagramIcon} width={33} height={33} fill="#FFFFFF"/>
+          <SVGIcon className="Footer__icon" svg={facebookIcon} width={33} height={33} fill="#FFFFFF"/>
+          <SVGIcon className="Footer__icon" svg={pinterestIcon} width={33} height={33} fill="#FFFFFF"/>
         </div>
       </div>
     );
