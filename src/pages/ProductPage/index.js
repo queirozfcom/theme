904fce43a633473afe754abf36ff5be8d3fac5e6
bellacorbo <bella.corbo@gmail.com>
2015-12-10 15:30:19 -0200

    Remove sku-selector files

--- src/pages/ProductPage/index.js
+++ src/pages/ProductPage/index.js
@@ -1,8 +1,6 @@
 import { actions } from 'sdk';
 import ProductPage from './ProductPage';
 import AddToCartButton from 'react-proxy?name=AddToCartButton!components/AddToCartButton/AddToCartButton';
-import SelectVariation from  'react-proxy?name=SelectVariation!components/Product/SelectVariation';
-import SkuSelector from  'react-proxy?name=SkuSelector!components/Product/SkuSelector';
 
 let components = [
   {
@@ -12,14 +10,6 @@ let components = [
   {
     name: 'AddToCartButton@vtex.storefront-theme',
     constructor: AddToCartButton
-  },
-  {
-    name: 'SkuSelector@vtex.storefront-theme',
-    constructor: SkuSelector
-  },
-  {
-    name: 'SelectVariation@vtex.storefront-theme',
-    constructor: SelectVariation
   }
 ];
 
