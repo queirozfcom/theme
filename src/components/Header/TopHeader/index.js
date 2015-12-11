e09d785e12f8c9555e44d115e58150ef28cb6ffe
Daniel Fosco <daniel.fosco@vtex.com.br>
2015-12-11 19:26:21 -0200

    Finish layout for TopHeader

--- src/components/Header/TopHeader/index.js
+++ src/components/Header/TopHeader/index.js
@@ -12,20 +12,27 @@ class TopHeader extends React.Component {
     let phoneNumberURL;
     phoneNumberURL = 'tel:' + phoneNumber;
 
-    let contactURL;
-    contactURL = '#';
+    let loginURL;
+    loginURL = '#';
+
+    let accountURL;
+    accountURL = '#';
 
     return (
       <div className="TopHeader">
-        <div className="TopHeader__wrap container">
+        <div className="container">
           <div className="col-md-2 col-lg-2">
-            <button className="TopHeader__social"><a href="">FB</a></button>
-            <button className="TopHeader__social"><a href="">IG</a></button>
+            <button className="TopHeader__button"><a href="">FB</a></button>
+            <button className="TopHeader__button"><a href="">IG</a></button>
           </div>
           <div className="TopHeader__contact col-md-10 col-lg-10">
             <div className="TopHeader__login">
-              <span><a className="TopHeader__login-link" href={contactURL}>Fale Conosco</a></span>
+              <span><a className="TopHeader__login-link" href={loginURL}>Login</a></span>
             </div>
+            <div className="TopHeader__login">
+              <span><a className="TopHeader__login-link" href={accountURL}>Minha Conta</a></span>
+            </div>
+
             <div className="TopHeader__phone-cta">
               <span className="TopHeader__phone-number">
                 <a href={phoneNumberURL} className="TopHeader__phone-url">
