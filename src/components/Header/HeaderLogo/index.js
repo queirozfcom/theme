00ca89db1895a2ff4a4d3cf4b29e4a3978d24c15
Daniel Fosco <danielfosco@gmail.com>
2015-12-15 17:34:45 -0200

    Add toggle for LogoImage or LogoText -- still some issues with SVG

--- src/components/Header/HeaderLogo/index.js
+++ src/components/Header/HeaderLogo/index.js
@@ -1,21 +1,33 @@
 import React from 'react';
 import { Link } from 'react-router';
 import './style.less';
-//import SVGIcon from 'utils/SVGIcon';
-//import facebookIcon from 'assets/icons/store_logo.svg';
-//import facebookImg from 'assets/icons/store_logo.png';
+import SVGIcon from 'utils/SVGIcon';
+import storeLogo from 'assets/images/store_logo.svg';
+import storeLogoImg from 'assets/images/store_logo.png';
 
 class HeaderLogo extends React.Component {
 
   render() {
 
-    //let logoImage = true;
+    let logoImage = false;
+
+    let logoImageMarkup = (
+      <a href="/">
+        <SVGIcon svg={storeLogo} fallback={storeLogoImg} height={45} fill="#153243" />
+      </a>
+    )
+
+    let logoTextMarkup = (
+      <h1 className="HeaderLogo__font">
+        <Link to="/">Modally</Link>
+      </h1>
+    );
+
+    let logoContent = logoImage ? logoImageMarkup : logoTextMarkup;
 
     return (
       <div className="HeaderLogo col-xs-7 col-sm-10 col-md-10 col-lg-10">
-        <h1 className="HeaderLogo__font">
-          <Link to="/">Modally</Link>
-        </h1>
+        {logoContent}
       </div>
 
     );
