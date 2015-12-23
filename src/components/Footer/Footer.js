f8f3bff63db4882e1de03de5c9259ccce5eb8693
afonso <afonsoinfo@gmail.com>
2015-12-23 12:47:06 -0200

    starts footer development

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,31 +1,49 @@
 import React from 'react';
+import { stores, connectToStores } from 'sdk';
 import './Footer.less';
 import SVGIcon from 'utils/SVGIcon';
 import instagramIcon from 'assets/icons/instagram.svg';
 import facebookIcon from 'assets/icons/facebook.svg';
 import pinterestIcon from 'assets/icons/pinterest.svg';
+import Categories from './Categories';
+//import { map } from 'lodash';
 
+@connectToStores()
 class Footer extends React.Component {
+
+  static getStores() {
+    return [
+      stores.CategoryStore
+    ];
+  }
+
+  static getPropsFromStores() {
+    return {
+      categories: stores.CategoryStore.getState()
+    };
+  }
+
   render() {
+    let items = this.props.categories.map((category) => {
+      console.log(category);
+      return (
+        <Categories cat={category} />
+      );
+    });
+
     return (
       <div className="Footer clearfix">
         <div className="Footer__wrap">
           <div className="Footer__social-icons">
-            <button className="Footer__button"><a href=""><SVGIcon svg={instagramIcon} width={33} height={33} fill="#FFFFFF"/></a></button>
-            <button className="Footer__button"><a href=""><SVGIcon svg={facebookIcon} width={33} height={33} fill="#FFFFFF"/></a></button>
-            <button className="Footer__button"><a href=""><SVGIcon svg={pinterestIcon} width={33} height={33} fill="#FFFFFF"/></a></button>
+            <button className="Footer__button"><a href="https://www.instagram.com/lojapilateslovers/"><SVGIcon svg={instagramIcon} width={33} height={33} fill="#703693"/></a></button>
+            <button className="Footer__button"><a href="https://www.facebook.com/LojaPilatesLovers"><SVGIcon svg={facebookIcon} width={33} height={33} fill="#703693"/></a></button>
           </div>
-
           <div className="Footer__links">
             <ul className="Footer__menu">
-              <li className="Footer__item"><a href="#" className="Footer__menu-link">Categorias</a></li>
-              <li className="Footer__item"><a href="#" className="Footer__menu-link">FAQ</a></li>
-              <li className="Footer__item"><a href="#" className="Footer__menu-link">Sobre</a></li>
-              <li className="Footer__item"><a href="#" className="Footer__menu-link">Termos de uso</a></li>
+              {items}
             </ul>
             <span className="Footer__address">Praia de Botafogo, 518, Sobreloja, Botafogo. Rio de Janeiro, RJ - 22250-040</span>
           </div>
-
         </div>
       </div>
     );
---
+++ src/pages/HomePage/HomePage.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import { actions, stores } from 'sdk';
+import Header from 'components/Header';
+import Footer from 'components/Footer/Footer';
+import Newsletter from 'components/Newsletter/Newsletter';
+import Policies from 'components/Policies/Policies';
+import './HomePage.less';
+
+const Area = stores.ComponentStore.state.getIn(['Area@vtex.storefront-sdk', 'constructor']);
+
+class HomePage extends React.Component {
+  componentWillMount() {
+    let currentURL = (window.location.pathname + window.location.search);
+    if (!stores.ResourceStore.getState().get(currentURL)) {
+      actions.AreaActions.getAreaResources({currentURL, id: 'home'});
+    }
+  }
+
+  render() {
+    return (
+      <div className="HomePage">
+        <Header areaPath="home" />
+        <div className="HomePage__wrapper">
+          <div className="HomePage__bannerarea">
+            <Area id="home/banner"/>
+          </div>
+          <div className="HomePage__shelfarea">
+            <Area id="home/shelf1"/>
+          </div>
+          <Policies/>
+        </div>
+        <Newsletter/>
+        <Area id="home/footer"/>
+      </div>
+    );
+  }
+}
+
+export default HomePage;
