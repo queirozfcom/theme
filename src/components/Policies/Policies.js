3c0e462b8fc7c4e1825f79ae3284eda9cf2593b0
afonso <afonsoinfo@gmail.com>
2015-12-23 15:14:25 -0200

    policies improvements

--- src/components/Policies/Policies.js
+++ src/components/Policies/Policies.js
@@ -12,47 +12,31 @@ class Policies extends React.Component {
   render() {
     return (
       <div className="Policies clearfix">
-        <h2 className="Policies__title">
-          Melhores Condições
-        </h2>
-
-        <div className="Policies__single clearfix">
-          <div className="col-xs-1 col-xs-offset-1">
-            <SVGIcon className="Policies__icon" svg={exchangeIcon}
-              fallback={exchangeImg} width={17} height={18} fill="#EDAF97"/>
-          </div>
+        <hr/>
+        <div className="Policies__single row-fluid clearfix">
           <h3 className="Policies__single-title col-xs-9">
-            Troca fácil
+            Entregamos em todo o Brasil
           </h3>
-
-          <div className="col-xs-10 col-xs-offset-1">
-            <p className="Policies__description">A gente dá 30 dias, após o recebimento, para você ter certeza de que a sua roupa ficou perfeita! E com certeza vai ficar.</p>
+          <div className="col-xs-12">
+            <p className="Policies__description">Através dos Correios.</p>
           </div>
         </div>
 
-        <div className="Policies__single clearfix">
-          <div className="col-xs-1 col-xs-offset-1 Policies__icon-wrapper">
-            <SVGIcon className="Policies__icon" svg={shippingIcon} fallback={shippingImg} width={20} height={22} fill="#EDAF97"/>
-          </div>
+        <div className="Policies__single row-fluid clearfix">
           <h3 className="Policies__single-title">
             Frete Grátis
           </h3>
-
-          <div className="col-xs-10 col-xs-offset-1">
-            <p className="Policies__description">Oferecemos frete grátis em qualquer compra a partir de 2 produtos. Para (quase) qualquer lugar do Brasil.</p>
+          <div className="col-xs-12">
+            <p className="Policies__description">Para a região sudeste, nas compras acima de R$ 149,00.</p>
           </div>
         </div>
 
-        <div className="Policies__single clearfix">
-          <div className="col-xs-1 col-xs-offset-1">
-            <SVGIcon className="Policies__icon" svg={giftIcon} fallback={giftImg} width={14} height={18} fill="#EDAF97" />
-          </div>
+        <div className="Policies__single row-fluid clearfix">
           <h3 className="Policies__single-title col-xs-9">
-            Mimos de aniversário
+            Parcelamos sua compra
           </h3>
-
-          <div className="col-xs-10 col-xs-offset-1">
-            <p className="Policies__description">Durante todo o mês do seu aniversário, você ganha 20% de desconto em qualquer produto da loja!</p>
+          <div className="col-xs-12">
+            <p className="Policies__description">Parcelamos em 3 vezes sem juros compras acima de R$ 99,00</p>
           </div>
         </div>
       </div>
