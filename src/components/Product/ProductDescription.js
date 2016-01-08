bfacb9f61ff9cea6b6c715b63503e30e62b04ca3
afonso <afonsoinfo@gmail.com>
2016-01-08 12:02:21 -0200

    starts product page customization

--- src/components/Product/ProductDescription.js
+++ src/components/Product/ProductDescription.js
@@ -3,17 +3,12 @@ import './ProductDescription.less';
 
 class Description extends React.Component {
   render() {
+    let description = this.props.description;
     return (
-      <div>
-        <div className="ProductDescription">
-            <h3 className="ProductDescription__title">Descrição</h3>
-            <div className="ProductDescription__text">
-              <p>A Modally acredita que o conforto faz um bom guarda-roupa. Queremos que você aproveite sua liberdade com simplicidade, qualidade e ao mesmo tempo tenha certeza que sua roupa é particulamente confortável de usar.</p>
-              <p>87% modal</p>
-              <p>10% seda</p>
-              <p>3% elastano</p>
-            </div>
-        </div>
+      <div className="ProductDescription row">
+          <div className="ProductDescription__text">
+            { description }
+          </div>
       </div>
     );
   }
