765017266423923e09dcac3a8f51c7ed3d2e6e8c
Daniel Fosco <daniel.fosco@vtex.com.br>
2016-01-12 14:29:39 -0200

    Fix rebase problems with the help of @tamorim

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -34,33 +34,40 @@ class Product extends React.Component {
     return (
       <div className="Product">
         <div className="row">
-          <Area id="product/product-image" images={defaultSku.images} />
+          <div className="col-xs-12 col-sm-12 col-md-6 col-lg-6">
+            <h2 className="Product__title hidden-sm hidden-md hidden-lg">{name}</h2>
+            <Area id="product/product-image" images={defaultSku.images} />
+          </div>
           <div className="Product__infos col-xs-12 col-sm-12 col-md-6 col-lg-6">
-            <h2 className="Product__title">{name}</h2>
+            <h2 className="Product__title visible-sm visible-md visible-lg">{name}</h2>
             <h3 className="Product__price "><Price value={price}/></h3>
+            <ProductDescription />
+            {
+              skus.length > 1 ?
+                <Area
+                  skus={skus}
+                  changeSelectedSku={this.changeSelectedSku}
+                  id="product/sku-selector"
+                /> : null
+            }
+            <AddToCartButton
+              skuId={defaultSku.id}
+              cartValidation={cartValidation}
+              className={className}
+              id="product-button"
+              route="product"
+            />
           </div>
         </div>
-        {
-          skus.length > 1 ?
+        <div className="row">
+          <div className="col-xs-12 col-sm-12 col-md-6 col-lg-6 pull-right Product__shipping-wrapper">
             <Area
-              skus={skus}
-              changeSelectedSku={this.changeSelectedSku}
-              id="product/sku-selector"
-            /> : null
-        }
-        <div className="col-xs-12 col-sm-12 col-md-6 col-lg-6 pull-right Product__shipping-wrapper">
-          <Area
-            sku={this.state.selectedSku}
-            id="product/shipping-calculator"
-          />
+              sku={this.state.selectedSku}
+              id="product/shipping-calculator"
+            />
+          </div>
+
         </div>
-        <AddToCartButton
-          skuId={defaultSku.id}
-          cartValidation={cartValidation}
-          className={className}
-          id="product-button"
-          route="product"
-        />
       </div>
     );
   }
