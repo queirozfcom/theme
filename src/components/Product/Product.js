6980c87aad7e7451922a5f549811a9ed4d61b138
Daniel Fosco <daniel.fosco@vtex.com.br>
2016-01-12 15:06:51 -0200

    Improve layout for product page

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -29,19 +29,23 @@ class Product extends React.Component {
     let price = defaultSku.offers[0].price;
     let skus = this.props.skus;
     let cartValidation = this.state.selectedSku.length === 1 ? true : false;
-    let className = 'AddToCartButton';
+    let className = 'AddToCartButton--fixed';
 
     return (
       <div className="Product">
         <div className="row">
           <div className="col-xs-12 col-sm-12 col-md-6 col-lg-6">
-            <h2 className="Product__title hidden-sm hidden-md hidden-lg">{name}</h2>
-            <Area id="product/product-image" images={defaultSku.images} />
+            <div className="hidden-sm hidden-md hidden-lg">
+              <h2 className="Product__title">{name}</h2>
+              <h3 className="Product__price"><Price value={price}/></h3>
+            </div>
+            <Area id="product/product-image" images={defaultSku.images} />              
           </div>
           <div className="Product__infos col-xs-12 col-sm-12 col-md-6 col-lg-6">
-            <h2 className="Product__title visible-sm visible-md visible-lg">{name}</h2>
-            <h3 className="Product__price "><Price value={price}/></h3>
-            <ProductDescription />
+            <div className="visible-sm visible-md visible-lg">
+              <h2 className="Product__title">{name}</h2>
+              <h3 className="Product__price "><Price value={price}/></h3>
+            </div>
             {
               skus.length > 1 ?
                 <Area
@@ -57,14 +61,15 @@ class Product extends React.Component {
               id="product-button"
               route="product"
             />
-          </div>
-        </div>
-        <div className="row">
-          <div className="col-xs-12 col-sm-12 col-md-6 col-lg-6 pull-right Product__shipping-wrapper">
             <Area
               sku={this.state.selectedSku}
               id="product/shipping-calculator"
             />
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-xs-12 col-sm-12 col-md-8 col-lg-8 Product__shipping-wrapper">
+            <ProductDescription />
           </div>
 
         </div>
