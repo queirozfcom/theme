385ff007d04081d66bb271b5c35c80b0018667ac
Thor Amorim <thoramorim@gmail.com>
2016-03-17 17:23:14 -0300

    Remove ResourceStore checks for data
    Unnecessary on the new routing model

--- src/components/HomePage/HomePage.js
+++ src/components/HomePage/HomePage.js
@@ -1,18 +1,11 @@
 import React from 'react';
-import { actions, stores } from 'sdk';
+import { stores } from 'sdk';
 import './HomePage.less';
 import Policies from 'components/Policies/Policies';
 
 const Placeholder = stores.ComponentStore.state.getIn(['Placeholder@vtex.storefront-sdk', 'constructor']);
 
 class HomePage extends React.Component {
-  componentDidMount() {
-    let currentURL = (window.location.pathname + window.location.search);
-    if (!stores.ResourceStore.getState().get(currentURL)) {
-      actions.AreaActions.getAreaResources({currentURL, id: 'home'});
-    }
-  }
-
   render() {
     return (
       <div className="HomePage">
