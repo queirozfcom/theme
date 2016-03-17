4a09690994764631387bd3a585eeaa6b8279c377
Thor Amorim <thoramorim@gmail.com>
2016-03-17 19:02:52 -0300

    Move policies from DefaultTemplate to HomePage

--- src/components/DefaultTemplate/DefaultTemplate.js
+++ src/components/DefaultTemplate/DefaultTemplate.js
@@ -2,7 +2,6 @@ import React from 'react';
 import Header from 'components/Header';
 import Footer from 'components/Footer/Footer';
 import Newsletter from 'components/Newsletter/Newsletter';
-import Policies from 'components/Policies/Policies';
 import { stores } from 'sdk';
 
 const Placeholder = stores.ComponentStore.state.getIn(['Placeholder@vtex.storefront-sdk', 'constructor']);
@@ -11,14 +10,12 @@ class DefaultTemplate extends React.Component {
   render() {
     return (
       <div className="DefaultTemplate">
-        <Header id={this.props.id} />
+        <Header />
           <Placeholder
             id="body"
             params={this.props.params}
             location={this.props.location}
-            parentId={this.props.id}
           />
-        <Policies/>
         <Newsletter/>
         <Footer/>
       </div>
